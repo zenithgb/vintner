6eee1a19ca5c32082b6c32293c9a8c80e26598ef
Zach Everson <zach@eversonconsulting.com>
2026-07-18 22:05:11 -0400

    Initial Fabric project

---
+++ src/client/java/com/zach2110/vintner/client/VintnerClient.java
@@ -0,0 +1,10 @@
+package com.zach2110.vintner.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VintnerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
