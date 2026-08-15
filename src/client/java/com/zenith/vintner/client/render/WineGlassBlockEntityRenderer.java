10c7b6bdc7ddde12901345c99aa58e257a10f97f
Zach Everson <zach@eversonconsulting.com>
2026-08-15 01:58:43 -0400

    Refine placed wine glass proportions

--- src/client/java/com/zenith/vintner/client/render/WineGlassBlockEntityRenderer.java
+++ src/client/java/com/zenith/vintner/client/render/WineGlassBlockEntityRenderer.java
@@ -96,7 +96,7 @@ public final class WineGlassBlockEntityRenderer implements
             // FIXED item models are centred around the render origin. Raise the
             // goblet so its foot, rather than the middle of its bowl, rests on
             // the supporting block surface.
-            poseStack.translate(position.x, 0.36, position.z);
+            poseStack.translate(position.x, 0.31, position.z);
             poseStack.scale(1.05F, 1.05F, 1.05F);
             state.glasses[index].submit(
                     poseStack,
